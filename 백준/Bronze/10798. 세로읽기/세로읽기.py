cace24e9d26fa32064300ef1faef7df4447eba58
Kim So Yeon <92496809+kssosoy@users.noreply.github.com>
2024-03-17 18:04:09 +0900

    [Bronze I] Title: 세로읽기, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -1,25 +1,14 @@
-# 입력된 문자열 중 가장 긴 것의 길이를 찾기 위한 변수 초기화
-max_len = 0
-
-# 입력 처리 및 가장 긴 문자열 길이 찾기
-arr = []
-for _ in range(5):
-    line = list(input())  # 바로 리스트로 변환
-    arr.append(line)
-    # 가장 긴 문자열의 길이 업데이트
-    if len(line) > max_len:
-        max_len = len(line)
-
-# 각 문자열의 길이를 가장 긴 것에 맞추어 빈 문자열로 패딩
+letter=[]
+str1=""
+maxnum=0
 for i in range(5):
-    arr[i] += [''] * (max_len - len(arr[i]))
-
-# 세로로 읽기
-pri_str = ""
-for j in range(max_len):  # 가장 긴 문자열의 길이만큼 반복
-    for i in range(5):
-        pri_str += arr[i][j]  # 세로로 문자 추가
-
-# 결과 출력
-print(pri_str)
-
+  a=list(input())
+  if len(a)>=maxnum:
+    maxnum=len(a)
+  letter.append(a)
+for m in range(5):
+  letter[m]+=['']*(maxnum-len(letter[m]))
+for j in range(maxnum):
+  for k in range(5):
+    str1+=letter[k][j]
+print(str1)
