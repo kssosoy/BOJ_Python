0849556662a9abc1780d48b5c17765d394fb7706
Kim So Yeon <92496809+kssosoy@users.noreply.github.com>
2024-06-05 11:26:25 +0900

    [Bronze III] Title: 공 넣기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.py
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -1,8 +1,7 @@
-import sys
-n,m=map(int,sys.stdin.readline().split())
-num=[0]*n
-for i in range(m):
-  a,b,c=map(int,sys.stdin.readline().split())
-  for i in range(a-1,b):
-      num[i]=c
-print(*num)
+a,b=map(int,input().split())
+num=[0]*a
+for i in range(0,b):
+    c,d,e= map(int,input().split())
+    for j in range(c,d+1):
+        num[j-1]=e
+print(' '.join(map(str, num)))
