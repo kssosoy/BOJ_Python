5b1b9ea019ffe1bd70f710211f43b93d771881ae
Kim So Yeon <92496809+kssosoy@users.noreply.github.com>
2024-06-05 10:13:23 +0900

    [Bronze V] Title: A+B - 5, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
@@ -1,8 +1,8 @@
 import sys
-while 1:
-  a,b=map(int,sys.stdin.readline().split())
-  if(a==0 and b==0):
-    break
-  else:
-    print(a+b)
-  
+
+while True:
+    a, b = map(int, sys.stdin.readline().split())
+    if a == 0 and b == 0:
+        break
+    else:
+        print(a + b)
