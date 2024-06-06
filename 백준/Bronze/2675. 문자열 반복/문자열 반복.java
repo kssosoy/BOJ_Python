8c80003522d92c5e43d07062eb8d01d6609309c1
Kim So Yeon <92496809+kssosoy@users.noreply.github.com>
2024-06-06 13:44:33 +0900

    [Bronze II] Title: 문자열 반복, Time: 224 ms, Memory: 17904 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int testcase = sc.nextInt();
+        for (int i=0; i< testcase; i++){
+            int R = sc.nextInt();
+            String S = sc.next();
+            for (int j=0; j<S.length(); j++){
+                for (int k =0; k<R; k++){
+                    System.out.print(S.charAt(j));
+                }
+            }
+            System.out.println();
+        }
+    }
+}
