a962dcc44d089eb7f6bf2a2806aa222a2375f401
Kim So Yeon <92496809+kssosoy@users.noreply.github.com>
2024-06-10 19:19:50 +0900

    [Silver V] Title: 색종이, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,14 +1,17 @@
-a=int(input())
-letter = [[0]*100 for _ in range(100)] 
-num=0
+a= int(input())
+count=0
+paper=[[0 for _ in range(100)] for _ in range(100)]
 for i in range(a):
-  b,c=map(int,input().split())
-  for j in range(b, b+10):
-   for k in range(c,c+10):
-     if letter[j][k]==0:
-       letter[j][k]=1
-     else:
-       pass
+    x,y=map(int,input().split())
+    for j in range(x,x+10):
+        for k in range(y,y+10):
+            if(paper[j][k]==1):
+                continue
+            else:
+                paper[j][k]=1
 for i in range(100):
-  num+=sum(letter[i])
-print(num)
+    for j in range(100):
+        if(paper[i][j]==1):
+            count+=1
+print(count)
+
