513b527bf9a22797c3aa7ea01514f05c0e2bc99e
Kim So Yeon <92496809+kssosoy@users.noreply.github.com>
2024-06-16 13:12:04 +0900

    [Bronze III] Title: 저작권, Time: 204 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A =sc.nextInt();
+        int I =sc.nextInt();
+        System.out.println(A*(I-1)+1);
+
+
+    }
+}
