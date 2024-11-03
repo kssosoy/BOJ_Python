3e0434f5ee9034d49a1c640c2d766441605b7cac
Kim So Yeon <92496809+kssosoy@users.noreply.github.com>
2024-11-03 18:02:20 +0900

    [Bronze V] Title: 개수 세기, Time: 100 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i =0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int b = Integer.parseInt(br.readLine());
+
+        for (int j=0; j<arr.length; j++){
+            if(b==arr[j]){
+                count++;
+            }
+        }
+        System.out.println(count);
+        bw.flush();
+        bw.close();
+        br.close();
+
+
+    }
+}
