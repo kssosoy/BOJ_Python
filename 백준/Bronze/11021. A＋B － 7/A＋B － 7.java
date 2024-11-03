4f1f135a9a3cdb6e3c569269a2acc1ae4c955878
Kim So Yeon <92496809+kssosoy@users.noreply.github.com>
2024-11-03 17:16:49 +0900

    [Bronze V] Title: A+B - 7, Time: 136 ms, Memory: 16104 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        for (int i=1; i<=a; i++){
+            st = new StringTokenizer(br.readLine());
+            System.out.println("Case #"+i+": "
+            +(Integer.parseInt(st.nextToken())
+            +Integer.parseInt(st.nextToken())));
+        }
+        br.close();
+
+
+    }
+}
