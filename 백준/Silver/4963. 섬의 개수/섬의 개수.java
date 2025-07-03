96fbb98dd4761b71f40ab691927fb467e4232de2
Kim So Yeon <92496809+kssosoy@users.noreply.github.com>
2025-07-03 14:26:37 +0900

    [Silver II] Title: 섬의 개수, Time: 144 ms, Memory: 15796 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,76 +1,62 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static int[][] graph;
+    static int w, h;
+    static int[][] map;
     static boolean[][] visited;
-    static int w,h;
+    static int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+    static int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
 
-    static int[] dx = {-1, 1, 0, 0, -1, -1, 1, 1};
-    static int[] dy = {0, 0, -1, 1, -1, 1, -1, 1};
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        while (true) {
-            st = new StringTokenizer(bf.readLine());
-            w = Integer.parseInt(st.nextToken());
-            h = Integer.parseInt(st.nextToken());
-            if(w==0 && h==0){
-                break;
-            }
-            else{
+    static void dfs(int y, int x) {
+        visited[y][x] = true;
 
-                graph=new int[h][w];
-                for(int i=0; i<h; i++){
-                    st = new StringTokenizer(bf.readLine());
-                    for(int j=0; j<w;j++){
-                        graph[i][j]=Integer.parseInt(st.nextToken());
-                    }
+        for (int i = 0; i < 8; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if (nx >= 0 && nx < w && ny >= 0 && ny < h) {
+                if (!visited[ny][nx] && map[ny][nx] == 1) {
+                    dfs(ny, nx);
                 }
             }
-            int count=0;
-            visited= new boolean[h][w];
-            for(int i=0; i<h; i++){
-                for(int j=0; j<w; j++){
-                    if(!visited[i][j]&&graph[i][j]==1){
-                        bfs(i,j);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            w = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
+            if (w == 0 && h == 0) break;
+
+            map = new int[h][w];            // 고침!
+            visited = new boolean[h][w];
+
+            for (int i = 0; i < h; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < w; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            int count = 0;
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    if (map[i][j] == 1 && !visited[i][j]) {
+                        dfs(i, j);
                         count++;
                     }
                 }
             }
+
             System.out.println(count);
-
         }
 
     }
-    static void bfs(int x, int y) {
-        visited[x][y] = true;
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[] {x, y});
-
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int cx = current[0];
-            int cy = current[1];
-            for (int i = 0; i < 8; i++) {
-                int nx = cx + dx[i];
-                int ny = cy + dy[i];
-                if (nx >= 0 && nx < h && ny >= 0 && ny < w) {
-                    if (!visited[nx][ny] && graph[nx][ny] == 1) {
-                        queue.add(new int[] {nx, ny});
-                        visited[nx][ny] = true;
-                    }
-                }
-            }
-        }
-    }
-
 }
