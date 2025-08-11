fab57230b5321ecc79b27d1e2d5337b9db785c53
Kim So Yeon <92496809+kssosoy@users.noreply.github.com>
2025-08-11 15:36:41 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 3156 ms, Memory: 129628 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -0,0 +1,14 @@
+n,m=map(int,input().split())
+arr=[]
+for _ in range(n):
+    arr.append(list(map(int,input().split())))
+
+dp = [[0] * (n+1) for _ in range(n+1)]
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + arr[i-1][j-1]
+
+for _ in range(m):
+    x1,y1,x2,y2=map(int, input().split())
+    result=dp[x2][y2]-dp[x1-1][y2]-dp[x2][y1-1]+dp[x1-1][y1-1]
+    print(result)
