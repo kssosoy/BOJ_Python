43f3100c4e5b85ba0928dc7d56cdf6efe58f4d9a
Kim So Yeon <92496809+kssosoy@users.noreply.github.com>
2026-01-07 12:03:51 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 87.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(int a, int b) {
+        String answer = "";
+        int[] months ={ 31,29,31,30,31,30,31,31,30,31,30,31};
+        String[] days = {"FRI", "SAT", "SUN","MON","TUE", "WED", "THU"};
+        int day =0;
+        int num=0;
+        for(int i=0; i<a-1; i++){
+            day+=months[i];
+        }
+        day+=b;
+        num = (day-1)%7;
+        answer = days[num];
+        return answer;
+    }
+}
