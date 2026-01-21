f3e8bdfda0836d36f217d5431db7ccb79e7c0c84
Kim So Yeon <92496809+kssosoy@users.noreply.github.com>
2026-01-21 15:26:44 +0900

    [level 2] Title: 전화번호 목록, Time: 355.50 ms, Memory: 98.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        Arrays.sort(phone_book);
+        for(int i=0; i<phone_book.length-1;i++){
+            String s =phone_book[i];
+        int len = s.length();
+            if(phone_book[i+1].startsWith(s)){
+                return false;
+            }
+        }
+        
+        return answer;
+    }
+}
